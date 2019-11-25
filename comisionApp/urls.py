b8d09ed150ca765ba25013905c12cf6db6605df4
poio90 <germanv09@gmail.com>
2019-11-25 11:35:37 -0300

    ahora si se agregaron los archivos estaticos

--- comisionApp/urls.py
+++ comisionApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,listarComisiones
 
 urlpatterns = [
-    path('', home, name='index')
+    path('', home, name='index'),
+    path('', listarComisiones, name='listarComisiones'),
 ]
